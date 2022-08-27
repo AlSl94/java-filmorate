6d40af5f5e4883d10919002bff42d3581e149968
Sergej Nishpal <sergej.nishpal@yandex.ru>
2022-08-28 02:58:00 +0300

    fix-develop: исправил sql-запрос в методе проверки жанра на существование в БД.

--- src/main/java/ru/yandex/practicum/filmorate/storage/genre/GenreDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/genre/GenreDbStorage.java
@@ -1,7 +1,6 @@
 package ru.yandex.practicum.filmorate.storage.genre;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 import ru.yandex.practicum.filmorate.exceptions.WrongParameterException;
@@ -10,31 +9,36 @@ import ru.yandex.practicum.filmorate.model.Genre;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
+import java.util.Objects;
 
 @Component
-public class GenreDbStorage implements GenreStorage{
+public class GenreDbStorage implements GenreStorage {
 
     private final JdbcTemplate jdbcTemplate;
+
     @Autowired
     public GenreDbStorage(JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
     }
+
     @Override
     public Genre getGenreById(Integer id) {
         return jdbcTemplate.queryForObject("SELECT GENRE_ID, GENRE FROM genres WHERE genre_id = ?",
                 this::mapRowToGenre, id);
     }
+
     @Override
     public List<Genre> allGenres() {
         return jdbcTemplate.query("SELECT GENRE_ID, GENRE FROM genres", this::mapRowToGenre);
     }
+
     @Override
     public void checkGenreExistence(Integer id) {
-        final String sqlQuery = "SELECT genre_id, genre FROM GENRES WHERE genre_id = ?";
-        try {
-            jdbcTemplate.queryForObject(sqlQuery, this::mapRowToGenre, id);
-        } catch (DataAccessException e) {
-            throw new WrongParameterException("Неверный genre.id");
+        final String sqlQuery = "SELECT COUNT(GENRE_ID) FROM GENRES WHERE GENRE_ID = ?";
+        int genresCount = Objects.requireNonNull(jdbcTemplate.queryForObject(sqlQuery, Integer.class, id));
+
+        if (genresCount == 0) {
+            throw new WrongParameterException("Несуществующий genre.id");
         }
     }
 
